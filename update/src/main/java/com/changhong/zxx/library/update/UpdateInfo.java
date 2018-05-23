b4eef5a90c43095d2abbc14146ba28795aa23a79
xin.zhang <517608831@qq.com>
2018-05-23 10:04:00 +0800

    增加字段

--- update/src/main/java/com/changhong/zxx/library/update/UpdateInfo.java
+++ update/src/main/java/com/changhong/zxx/library/update/UpdateInfo.java
@@ -11,6 +11,7 @@ public class UpdateInfo {
     public UpdateManager.UpdateType type = UpdateManager.UpdateType.NOUPDATE;//当前更新类型
     public String url = "";   //后台下发的更新地址，为空会去取 url_backup =”备用升级地址”
     public String app_version = "";     //升级后版本号
+    public String tip;//版本说明
 
     @Override
     public String toString() {
